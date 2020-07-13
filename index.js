d36deccb3807b4fe5eac0b8d942f8d5a379d5070
Shubham <smartshubhampathak@gmail.com>
2020-07-13 19:47:12 +0530

    Finished project

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //Importing modules
 const express= require('express');
 const bodyParser=require('body-parser');
+const sqlite3 = require('sqlite3').verbose()
 
 const app= express(); // initialising object of express
+
+
+//connection with database
+const db = new sqlite3.Database('./Database/articles.db',function(){
+    console.log("Database successfully connected")
+})
+
+//setting my view engine as ejs
+app.set('view engine', 'ejs');
 
 //Using body parser
 app.use(bodyParser.urlencoded({extended:true}));
@@ -10,29 +20,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 //For sending css,js,images and any other static files along with html
 //telling express to look for statics file in 'public' folder
 app.use(express.static('public'))
-
-//Database
-var Articles=[
-    {
-        id:1,
-        title:"Nodejs",
-        body:"Node js is an open source javascript runtime"
-
-    },
-    {
-        id:2,
-        title:"Flutter",
-        body:"Flutter is a UI sdk for cross platformmobile development"
-
-    },
-    {
-        id:3,
-        title:"React",
-        body:"Front end framework"
-
-    },
-
-]
 
 
 //home route
@@ -43,7 +30,12 @@ app.get('/',function(req,res){
 
 //articles route
 app.get('/articles',function(req,res){
-    res.sendFile(__dirname+"/views/articles.html");
+
+    //Fetching all the articles from database
+    db.all('select * from Articles;',function(err,rows){
+        if(err) console.log(err);
+        res.render('articles',{Articles:rows});
+    })  
 })
 
 
@@ -52,15 +44,27 @@ app.get('/articles/:id',function(req,res){
     //extracting info from url parameters
     var article_id=req.params.id;
 
-    //depending upon the id send different html files
-    if(article_id==1){
-        res.sendFile(__dirname+"/views/nodejs.html");
-    }else if(article_id==2){
-        res.sendFile(__dirname+"/views/flutter.html");
-    }
-    else if(article_id==3){
-        res.sendFile(__dirname+"/views/react.html");
-    }
+    //Fetching the article with a particular id
+    db.each('select * from Articles where id=?;',[article_id],function(err,row){
+        if(err) console.log(err);
+        res.render('singleArticle.ejs', {article: row})
+    })
+})
+
+app.delete('/articles/:id',function(req,res){
+    //extracting info from url parameters
+    var article_id=req.params.id;
+
+    //deleting the article with particular id
+    db.run('delete from Articles where id=?;',[article_id],function(err){
+        if(err) console.log(err);
+        console.log("Deleted article");
+
+        //sending the status in response as we can't redirect in case of delete and put requests 
+        //instead we send back the status code.
+        res.sendStatus(200);
+    })
+
 })
 
 
@@ -68,19 +72,16 @@ app.get('/articles/:id',function(req,res){
 app.get('/newArticle',function(req,res){
     res.sendFile(__dirname+"/views/newArticle.html")
 })
+
 //When someone submits a new article
 app.post('/newArticle',function(req,res){
     //extracting the information send by html form
-    var newArticle={
-        id:Articles.length+1,
-        title:req.body.title,
-        body:req.body.body
-    }
-
-    //adding the new article in my database
-    Articles.push(newArticle);
-    console.log(Articles);
-
+    //using req.body.title and req.body.body
+    //and inserting the data in database 
+    db.run('insert into Articles(title,body) values (?,?);',[req.body.title,req.body.body],function(err){
+        if(err) console.log(err);
+        console.log("Inserted successfully");
+    })
     //redirecting the client to articles route in response
     res.redirect('/articles');
 })
